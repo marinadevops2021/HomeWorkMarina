dded9fa48b9fe424c8451af2e2992b25551b4da4
Marina Minasov <87994268+marinadevops2021@users.noreply.github.com>
2022-02-02 14:59:32 +0200

    change main.tf

--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -9,4 +9,19 @@ terraform {
     required_version = ">= 0.14.10"
 }
 
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "Terraform-Test-ec2" {
+  ami           = "ami-0ed9277fb7eb570c9"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HomeWorkMarinaDev"
+    Terraform = "True"
+  }
+}
+
 
